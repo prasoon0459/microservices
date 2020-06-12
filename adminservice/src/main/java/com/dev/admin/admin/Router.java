07e8fead8048cac837dfc39a00ffe1087a5c7d50
aayush-ag21 <aayush.ag21@gmail.com>
2020-06-12 17:28:39 +0530

    incorrect admin

--- adminservice/src/main/java/com/dev/admin/admin/Router.java
+++ adminservice/src/main/java/com/dev/admin/admin/Router.java
@@ -3,6 +3,8 @@ package com.dev.admin.admin;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityManager;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,33 +24,49 @@ public class Router {
     @PostMapping(value="/user")
     public String create(@RequestBody final User rUser){
         try{
+            /*
+            EntityManager em = UserRepository.createEntityManager();
+            em.getTransaction().begin();
+            
+            Author a = new Author();
+            a.setFirstName("Thorben");
+            a.setLastName("Janssen");
+            a.setCategory(Category.NON_FICTION);
+            a.setPseudonym("Thorben Janssen");
+            em.persist(a);
+            
+            em.getTransaction().commit();
+            em.close();
+            */
             userRepository.save(rUser);
             return "Added";
-        } catch (final Exception e) {
-            return "Already Exists or Insufficient Data";
+        }catch (final Exception e){
+            return e.toString();
+            //return "Already Exists or Incorrect Data";
         }
     }
-
+    
     @GetMapping(value = "/user")
     public List<User> read() {
         return userRepository.findAll();
     }
-
+    
     @PutMapping(value = "/user")
-    public String update(@RequestBody final User rUser) {
+    public String update(@RequestBody final User rUser){
         try {
             final Optional<User> lUser = userRepository.findByUsername(rUser.getUsername());
             if (lUser.isEmpty()) {
                 return "User does not exist";
             } else {
-               //Insert Logic
+                lUser.get().setRoles(rUser.getRoles());
+                lUser.get().setValid(rUser.getValid());
                 return "Updated";
             }
         } catch (final Exception e) {
             return e.toString();
         }
     }
-
+    
     @DeleteMapping(value = "/user/{rUsername}")
     public String delete(@PathVariable("rUsername") String rUsername) {
         try {
