49fcde68fe41e2211c10528f9a802457d5ecb64b
aayush-ag21 <aayush.ag21@gmail.com>
2020-06-14 22:03:25 +0530

    Checkbox

--- frontend/verified/tableRequests.js
+++ frontend/verified/tableRequests.js
@@ -5,12 +5,12 @@ function initiate(){
         table.DataTable({
             data: response, 
             columns: [
-            {"data":"username"},
-            {"data":"name"},
-            {"data":"phone"},
-            {"data":"password"},
-            {"data":"valid"},
-            {"data":"roles"}
+                {"data":"username"},
+                {"data":"name"},
+                {"data":"phone"},
+                {"data":"password"},
+                {"data":"valid"},
+                {"data":"roles"}
             ]
         });
     },
@@ -18,13 +18,13 @@ function initiate(){
         401: function() {
             if(refresh()){
                 console.log("Token was refreshed");
-                add();
+                initiate();
             }
             else{
                 console.log("Token was not refreshed");
             }
         }
-      },
+    },
     error: function (xhr, status){
         console.log(xhr);
     }});
@@ -38,6 +38,13 @@ function add(){
     if($('#add_userrole').val()){
         this_role.push("ROLE_USER");
     }
+    let this_valid;
+    if($('#add_valid').is(":checked")){
+        this_valid=true;
+    }
+    else{
+        this_valid=false;
+    };
     $.ajax({url:"http://10.167.80.144/user",'type': "POST",
     data:JSON.stringify({
         username:$('#add_username').val(),
@@ -61,94 +68,101 @@ function add(){
                     console.log("Token was not refreshed");
                 }
             }
-          },
+        },
         error: function (xhr, status){
             console.log(xhr);
         }
     });
+    
+    table.DataTable().row.add({
+        "username":$('#add_username').val(),
+        "name":$('#add_name').val(),
+        "phone":$('#add_phone').val(),
+        "password":CryptoJS.MD5($('#add_password').val()).toString(CryptoJS.enc.Base64),
+        "valid":$('#add_valid').val(),
+        "roles":this_role.toString()}).draw();
         
-        table.DataTable().row.add({
-            "username":$('#add_username').val(),
-            "name":$('#add_name').val(),
-            "phone":$('#add_phone').val(),
-            "password":CryptoJS.MD5($('#add_password').val()).toString(CryptoJS.enc.Base64),
-            "valid":$('#add_valid').val(),
-            "roles":this_role.toString()}).draw();
-            
-            closeadd();
-            
-        }//end of add
+        closeadd();
         
-        function update(){
-            let this_role=[];
-            if($('#update_adminrole').val()){
-                this_role.push("ROLE_ADMIN");
-            }
-            if($('#update_userrole').val()){
-                this_role.push("ROLE_USER");
-            }
-            $.ajax({url:"http://10.167.80.144/user",'type': "PUT",
-            data:JSON.stringify({
-                username:$('#update_username').val(),
-                name:$('#update_name').val(),
-                phone:$('#update_phone').val(),
-                password:CryptoJS.MD5($('#update_password').val()).toString(CryptoJS.enc.Base64),
-                valid:$('#update_valid').val(),
-                role:this_role.toString()}),
-                'contentType':"application/json",
-                dataType: "json",
-                success: function (response){
-                    console.log(respone);
-                },
-                statusCode: {
-                    401: function() {
-                        if(refresh()){
-                            console.log("Token was refreshed");
-                            add();
-                        }
-                        else{
-                            console.log("Token was not refreshed");
-                        }
-                    }
-                  },
-                error: function (xhr, status){
-                    console.log(xhr);
-                   }
-                });
-                closeupdate();
-                //https://stackoverflow.com/questions/38392464/how-to-find-a-specific-row-by-values-in-jquery-datatables
-            }
-            
-            
-            function del(){
-                $.ajax({url:"http://10.167.80.144/user/"+$('#delete_username').val(),'type':"DELETE",
-                success: function (response){
-                    var resp = response;
-                    console.log(resp);
-                },
-                statusCode: {
-                    401: function() {
-                        if(refresh()){
-                            console.log("Token was refreshed");
-                            add();
-                        }
-                        else{
-                            console.log("Token was not refreshed");
-                        }
-                    }
-                  },
-                error: function (xhr, status){
-                    refresh();
-                    if(refresh_success){
+    }//end of add
+    
+    function update(){
+        let this_role=[];
+        if($('#update_adminrole').val()){
+            this_role.push("ROLE_ADMIN");
+        }
+        if($('#update_userrole').val()){
+            this_role.push("ROLE_USER");
+        }
+        let this_valid;
+        if($('#update_valid').is(":checked")){
+            this_valid=true;
+        }
+        else{
+            this_valid=false;
+        };
+        $.ajax({url:"http://10.167.80.144/user",'type': "PUT",
+        data:JSON.stringify({
+            username:$('#update_username').val(),
+            name:$('#update_name').val(),
+            phone:$('#update_phone').val(),
+            password:CryptoJS.MD5($('#update_password').val()).toString(CryptoJS.enc.Base64),
+            valid:$('#update_valid').val(),
+            role:this_role.toString()}),
+            'contentType':"application/json",
+            dataType: "json",
+            success: function (response){
+                console.log(respone);
+            },
+            statusCode: {
+                401: function() {
+                    if(refresh()){
                         console.log("Token was refreshed");
-                        del();
+                        update();
                     }
                     else{
                         console.log("Token was not refreshed");
                     }
-                }});
-                closedelete();
-                console.log(row(search($('#delete_username').val())));
-                //table.row(search($('#delete_username').val())).remove().draw();
-                //https://stackoverflow.com/questions/38392464/how-to-find-a-specific-row-by-values-in-jquery-datatables
+                }
+            },
+            error: function (xhr, status){
+                console.log(xhr);
             }
+        });
+        closeupdate();
+        //https://stackoverflow.com/questions/38392464/how-to-find-a-specific-row-by-values-in-jquery-datatables
+    }
+    
+    
+    function del(){
+        $.ajax({url:"http://10.167.80.144/user/"+$('#delete_username').val(),'type':"DELETE",
+        success: function (response){
+            var resp = response;
+            console.log(resp);
+        },
+        statusCode: {
+            401: function() {
+                if(refresh()){
+                    console.log("Token was refreshed");
+                    del();
+                }
+                else{
+                    console.log("Token was not refreshed");
+                }
+            }
+        },
+        error: function (xhr, status){
+            refresh();
+            if(refresh_success){
+                console.log("Token was refreshed");
+                del();
+            }
+            else{
+                console.log("Token was not refreshed");
+            }
+        }});
+        closedelete();
+        console.log(row(search($('#delete_username').val())));
+        //table.row(search($('#delete_username').val())).remove().draw();
+        //https://stackoverflow.com/questions/38392464/how-to-find-a-specific-row-by-values-in-jquery-datatables
+    }
