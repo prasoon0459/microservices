0a1e7e473e0c82e3bfc3b27d7f26331e43c0f715
aayush-ag21 <aayush.ag21@gmail.com>
2020-06-16 00:18:25 +0530

    time_stamp added

--- frontend/verified/refresh.js
+++ frontend/verified/refresh.js
@@ -3,16 +3,17 @@ function refresh(){
     $.ajax({'url':"http://10.167.80.144/auth/refresh",'type': "POST",
      'contentType':"application/json",dataType: "json",
     success: function (response) {
-        sessionStorage.setItem("loggedin",true);
         sessionStorage.setItem("response_username",response.username);
         sessionStorage.setItem("response_phone",response.phone);
         sessionStorage.setItem("response_username",response.username);
         sessionStorage.setItem("response_roles",response.roles[0]);
         console.log(response.roles[0]);
         console.log(response.roles[1]);
+        console.log('refresh success')
         return true;
     },
     error: function (xhr, status){
+        console.log('refresh failed');
         return false;
     }});
 }
