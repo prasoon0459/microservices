210ef8b50faee3f26503f31dd1c66c0f1ee7b77c
aayush-ag21 <aayush.ag21@gmail.com>
2020-06-16 01:46:18 +0530

    logout

--- frontend/logout.js
+++ frontend/logout.js
@@ -1,7 +1,6 @@
 function logout(){
     console.log('logout called');
     $.ajax({'url':"http://10.167.80.144/auth/refresh/logout",'type': "POST",
-    'contentType':"application/json",
     success: function (response){
         console.log('logout');
         sessionStorage.clear();
